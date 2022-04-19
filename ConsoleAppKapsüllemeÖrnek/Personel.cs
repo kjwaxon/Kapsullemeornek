16deab926a6bacd217074cecca2211bb8e59d901
kjwaxon <kaanaslan1996@hotmail.com>
2022-04-19 14:47:20 +0300

    Kapsülleme örnek

---
+++ ConsoleAppKapsüllemeÖrnek/Personel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppKapsüllemeÖrnek
+{
+    class Personel
+    {
+        public string isim;
+        public string unvan;
+
+        public void setIsım(string _isim)
+        {
+            isim = _isim;
+        }
+
+        public string getIsim()
+        {
+            return isim;
+        }
+        public void setUnvan(string _unvan)
+        {
+            unvan = _unvan;
+
+        }
+        public string getUnvan()
+        {
+            return unvan;
+        }
+    }
+}
---
+++ ConsoleAppKapsüllemeÖrnek/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppKapsüllemeÖrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel p = new Personel();
+            p.setIsım("Ahmet");
+            string gelen = p.getIsim();
+            p.setUnvan("Müdür");
+            string gelen2 = p.getUnvan();
+
+
+
+            if(gelen == "Ahmet" && gelen2 == "Müdür")
+            {
+                Console.WriteLine("Başarılı");
+            }
+            else
+
+            {
+                Console.WriteLine("Başarısız");
+            }
+            Console.ReadKey();
+        }
+    }
+}
